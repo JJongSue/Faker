db0e2b21d7c5f03b518188ab768e59d8b8579239
hayeon123 <selee3711@gmail.com>
2020-05-31 11:11:09 +0900

    style: vuetify 적용

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="nav">
         <div class="contents">
-            <router-link
-                to="/login">
-           <button @click="signIn">Sign In</button>
-            </router-link>
-           <button @click="signUp">Get started</button>
+          <v-row justify="space-btween">
+            <v-col cols="12" sm="6" md="2">
+              <router-link to="/"><h1>Faker</h1></router-link>
+            </v-col>
+            <v-col cols="12" sm="6" md="2">
+              <router-link to="/login"> <button @click="signIn">Sign In</button> </router-link>
+              <button @click="signUp">Get started</button>
+            </v-col>
+          </v-row>
         </div>
     </div>
 </template>
@@ -37,13 +41,20 @@ export default {
     height: 90px;
     color: #fff;
     border-bottom: 1px solid #aaa;
+
 }
 .nav .contents{
-    margin-left: 5%;
+    /* margin-left: 5%; */
     width:90%;
     height: 90px;
     background: #aaa;
     color:#fff;
-    position: absolute;
+    overflow: hidden;
+    position: relative;
+    margin:0 auto;
 }
+.contents .h1{
+  float: left;
+}
+
 </style>
