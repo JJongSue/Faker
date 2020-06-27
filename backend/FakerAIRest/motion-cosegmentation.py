5f494911cdd1e85380d6a938ddf18c0d470a6b1c
JJong Sue <jspark940321@gmail.com>
2020-06-27 20:54:29 +0900

    feat: 얼굴 변환 알고리즘
    얼굴 변환 알고리즘 넣는 작업

---
+++ backend/FakerAIRest/motion-cosegmentation.py
@@ -0,0 +1,36 @@
+
+
+
+def load_checkpoints(config, checkpoint, blend_scale=0.125, first_order_motion_model=False, cpu=False):
+    with open(config) as f:
+        config = yaml.load(f)
+
+    reconstruction_module = PartSwapGenerator(blend_scale=blend_scale,
+                                              first_order_motion_model=first_order_motion_model,
+                                              **config['model_params']['reconstruction_module_params'],
+                                              **config['model_params']['common_params'])
+
+    if not cpu:
+        reconstruction_module.cuda()
+
+    segmentation_module = SegmentationModule(**config['model_params']['segmentation_module_params'],
+                                             **config['model_params']['common_params'])
+    if not cpu:
+        segmentation_module.cuda()
+
+    if cpu:
+        checkpoint = torch.load(checkpoint, map_location=torch.device('cpu'))
+    else:
+        checkpoint = torch.load(checkpoint)
+
+    load_reconstruction_module(reconstruction_module, checkpoint)
+    load_segmentation_module(segmentation_module, checkpoint)
+
+    if not cpu:
+        reconstruction_module = DataParallelWithCallback(reconstruction_module)
+        segmentation_module = DataParallelWithCallback(segmentation_module)
+
+    reconstruction_module.eval()
+    segmentation_module.eval()
+
+    return reconstruction_module, segmentation_module
